9c8eb0d027a647711519af5b11eae1cdc5f6edf4
Ranjan <skillsapphire@gmail.com>
2023-04-08 17:19:28 +0200

    vercel deploy

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    MONGO_DB: "mongodb://127.0.0.1:27017/rentalapp",
+    //MONGO_DB: "mongodb://127.0.0.1:27017/rentalapp",
+    MONGO_DB: process.env.MONGO_DB,
     //MONGO_DB: "mongodb+srv://rentapp:hhbnnU7bCNsEe2Ny@cluster0.orgh0.mongodb.net/rentalapp?retryWrites=true&w=majority",
     JWT_SECRET: "CBDHBCIECOIN@#$%^)(!@#&^%%%SBHHSAJOK"
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+require('dotenv').config();
 global.__basedir = __dirname;
 
 const mongoose = require('mongoose');
